99a50d0532a3c6d83454fce7ff9b511d7435e2b6
ahmedessamrizk <ahmed.essam7722@gmail.com>
2024-04-29 19:27:56 +0300

    modify project

--- index.js
+++ index.js
@@ -2,13 +2,29 @@ import dotenv from 'dotenv'
 dotenv.config({ path: './config/.env' })
 import express from 'express'
 import * as indexRouter from './modules/index.router.js'
-
-
+import cors from 'cors'
+import { connectDB } from './DB/connection.js'
+import { InitServer } from './admin/admin.js';
 
 const app = express();
 let port;
 
-indexRouter.appRouter(app)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+
+app.use(`${process.env.baseURL}/auth`, indexRouter.authRouter);
+app.use(`${process.env.baseURL}/user`, indexRouter.userRouter);
+app.use(`${process.env.baseURL}/product`, indexRouter.productRouter);
+
+app.use('*', (req, res) => {
+    return res.status(404).json({ message: 'Not Found' });
+}
+);
+
+connectDB();
+InitServer();
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
--- modules/index.router.js
+++ modules/index.router.js
@@ -1,29 +1,8 @@
 import authRouter from "./auth/auth.router.js";
 import userRouter from "./user/user.router.js";
 import productRouter from "./product/product.router.js";
-import { connectDB } from "../DB/connection.js";
-import { InitServer } from "../admin/admin.js";
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-dotenv.config({ path: './config/.env' })
-
-export const appRouter = (app) => {
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }))
-    app.use(cors());
-
-    app.use(`/api/v1/auth`, authRouter);
-    app.use(`/api/v1/user`, userRouter);
-    app.use(`/api/v1/product`, productRouter);
-
-    app.use('*', (req, res) => {
-        return res.status(404).json({ message: 'Not Found' });
-    }
-    );
-
-    connectDB();
-    InitServer();
 
 
+export{
+    authRouter, productRouter, userRouter
 }
